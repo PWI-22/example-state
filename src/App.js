859413d26a213059dcf0a64083cca39299e6f68c
Roberto Padilha <robertocesardasilva@gmail.com>
2022-08-12 21:16:20 -0300

    Aula 11/08

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends React.Component{
         
         this.state = {
             message: '',
-            value: 0
+            value: 0,
+            userOption: 'opcao1'
         }
     }
 
@@ -34,6 +35,20 @@ class App extends React.Component{
         })
     }
 
+    onChangeValue(event) {
+        const newValue = event.target.value
+        this.setState({
+            value: newValue
+        })   
+    }
+
+    onChangeOption(event) {
+        const newOption = event.target.value
+        this.setState({
+            userOption: newOption
+        })
+    }
+
     render() { 
         console.log('App Render()')
         
@@ -45,9 +60,36 @@ class App extends React.Component{
                 <br />
 
                 Valor: <br />
-                <input type="number" min={0} max={10}/>
+                <input type="number" min={0} max={10}
+                    onChange={(event) => this.onChangeValue(event)} />
+                {this.state.value}
+                
+                <br /><br />
+
+                <label>
+                    <input type="radio" value="opcao1" 
+                        checked={this.state.userOption === 'opcao1'}
+                        onChange={(event) => this.onChangeOption(event)}/> Opção 1 
+                </label> 
+                <br />
+                
+                <label>
+                    <input type="radio" value="opcao2" 
+                        checked={this.state.userOption === 'opcao2'}
+                        onChange={(event) => this.onChangeOption(event)}/> Opção 2
+                </label>
+                <br />
+                
+                <label>
+                    <input type="radio" value="opcao3" 
+                        checked={this.state.userOption === 'opcao3'}
+                        onChange={(event) => this.onChangeOption(event)}/> Opção 3
+                </label>
 
                 <br />
+                Opção Selecionada: {this.state.userOption}
+
+                <br /><br />
                 <input type="button" value="Enviar Informações"
                     onClick={() => this.sendInfo() } />
             </div>
